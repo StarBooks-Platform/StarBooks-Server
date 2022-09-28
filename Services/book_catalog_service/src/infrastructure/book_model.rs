02130e6c42f9b3c42a958d5d186f9504975f3821
WarriorsSami <sbarbutdica@gmail.com>
2022-09-28 13:05:41 +0300

    STAR-8 feat(infrastructure): create book model for mongodb collection

---
+++ Services/book_catalog_service/src/infrastructure/book_model.rs
@@ -0,0 +1,43 @@
+use mongodb::bson::Uuid;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BookModel {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
+    pub id: Option<Uuid>,
+    pub isbn: String,
+    pub title: String,
+    pub publisher: PublisherModel,
+    pub authors: Option<Vec<AuthorModel>>,
+    pub short_description: String,
+    pub long_description: String,
+    pub year: u32,
+    pub num_pages: u32,
+    #[serde(rename = "image_blob")]
+    pub cover_image: Option<String>,
+    pub price: f32,
+    pub genre: GenreModel,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublisherModel {
+    pub name: String,
+    pub address: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthorModel {
+    #[serde(rename = "firstname")]
+    pub first_name: String,
+    #[serde(rename = "lastname")]
+    pub last_name: String,
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
+pub enum GenreModel {
+    Unknown = 0,
+    ScienceFiction = 1,
+    Science = 2,
+    Fiction = 3,
+    NonFiction = 4,
+}
