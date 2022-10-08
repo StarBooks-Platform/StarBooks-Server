e9bd141c13eb7d6473f06082a8cd5e6da65f3d58
WarriorsSami <sbarbutdica@gmail.com>
2022-10-08 14:32:03 +0300

    SBP-3 perf(domain): add more vvos for book fields and associated validation error types

--- Services/book_catalog_service/src/domain/core/errors.rs
+++ Services/book_catalog_service/src/domain/core/errors.rs
@@ -1,7 +1,7 @@
 use std::error;
 use std::fmt::{Display, Formatter, Result};
+use derive_more::{Display, Error};
 
-// TODO: define ValidationErro as en enum with different error types
 #[derive(Debug, Clone)]
 pub struct ValidationError {
     pub message: String,
@@ -16,5 +16,66 @@ impl Display for ValidationError {
 impl error::Error for ValidationError {
     fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         None
+    }
+}
+
+#[derive(Debug, Clone, Display, Error)]
+pub enum ValidationErrorType {
+    #[display(fmt = message)]
+    Id {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Isbn {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Title {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Author {
+        message: String,
+    },
+    #[display(fmt = message)]
+    ShortDescription {
+        message: String,
+    },
+    #[display(fmt = message)]
+    LongDescription {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Price {
+        message: String,
+    },
+    #[display(fmt = message)]
+    NumPages {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Year {
+        message: String,
+    },
+    #[display(fmt = message)]
+    Publisher {
+        message: String,
+    }
+}
+
+impl ValidationErrorType {
+    pub fn name(&self) -> String {
+        match self {
+            ValidationErrorType::Id { .. } => "IdValidationError".to_string(),
+            ValidationErrorType::Isbn { .. } => "IsbnValidationError".to_string(),
+            ValidationErrorType::Title { .. } => "TitleValidationError".to_string(),
+            ValidationErrorType::Author { .. } => "AuthorValidationError".to_string(),
+            ValidationErrorType::ShortDescription { .. } => "ShortDescriptionValidationError".to_string(),
+            ValidationErrorType::LongDescription { .. } => "LongDescriptionValidationError".to_string(),
+            ValidationErrorType::Price { .. } => "PriceValidationError".to_string(),
+            ValidationErrorType::NumPages { .. } => "PagesValidationError".to_string(),
+            ValidationErrorType::Year { .. } => "YearValidationError".to_string(),
+            ValidationErrorType::Publisher { .. } => "PublisherValidationError".to_string(),
+        }
     }
 }
--- Services/book_catalog_service/src/domain/core/vvos.rs
+++ Services/book_catalog_service/src/domain/core/vvos.rs
@@ -1,9 +1,10 @@
 use std::fmt;
 use std::fmt::{Display, Formatter};
 use domain_patterns::models::ValueObject;
+use chrono::Datelike;
 use crate::domain::core::errors::ValidationError;
 
-/// Range based length alphanumeric string vvo type
+/// Range based length string vvo type
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RblStringVvo<const MIN_LENGTH: usize, const MAX_LENGTH: usize> {
     value: String,
@@ -42,5 +43,123 @@ impl<const MIN_LENGTH: usize, const MAX_LENGTH: usize> Display
 for RblStringVvo<MIN_LENGTH, MAX_LENGTH> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.value.as_str())
+    }
+}
+
+/// Positive integer vvo type
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct PositiveIntVvo {
+    value: u32,
+}
+
+impl TryFrom<u32> for PositiveIntVvo {
+    type Error = ValidationError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        Self::validate(&value)?;
+        Ok(PositiveIntVvo { value })
+    }
+}
+
+impl ValueObject<u32> for PositiveIntVvo {
+    type ValueError = ValidationError;
+
+    fn validate(value: &u32) -> Result<(), Self::ValueError> {
+        if value <= &0 {
+            return Err(ValidationError {
+                message: "Value must be greater than 0".to_string()
+            });
+        }
+
+        Ok(())
+    }
+
+    fn value(&self) -> u32 {
+        self.value
+    }
+}
+
+impl Display for PositiveIntVvo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+/// Positive float vvo type
+#[derive(Debug, Clone, PartialEq)]
+pub struct PositiveFloatVvo {
+    value: f64,
+}
+
+impl TryFrom<f64> for PositiveFloatVvo {
+    type Error = ValidationError;
+
+    fn try_from(value: f64) -> Result<Self, Self::Error> {
+        Self::validate(&value)?;
+        Ok(PositiveFloatVvo { value })
+    }
+}
+
+impl ValueObject<f64> for PositiveFloatVvo {
+    type ValueError = ValidationError;
+
+    fn validate(value: &f64) -> Result<(), Self::ValueError> {
+        if value <= &0.0 {
+            return Err(ValidationError {
+                message: "Value must be greater than 0".to_string()
+            });
+        }
+
+        Ok(())
+    }
+
+    fn value(&self) -> f64 {
+        self.value
+    }
+}
+
+impl Display for PositiveFloatVvo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+/// Release year vvo type
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ReleaseYearVvo<const START_YEAR: usize> {
+    value: u32,
+}
+
+impl<const START_YEAR: usize> TryFrom<u32> for ReleaseYearVvo<START_YEAR> {
+    type Error = ValidationError;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        Self::validate(&value)?;
+        Ok(ReleaseYearVvo { value })
+    }
+}
+
+impl<const START_YEAR: usize> ValueObject<u32> for ReleaseYearVvo<START_YEAR> {
+    type ValueError = ValidationError;
+
+    fn validate(value: &u32) -> Result<(), Self::ValueError> {
+        let current_year = chrono::Utc::now().year() as u32;
+        if value < &(START_YEAR as u32) || value > &current_year {
+            return Err(ValidationError {
+                message: format!("Value must be between {} and {}", START_YEAR, current_year)
+            });
+        }
+
+        Ok(())
+    }
+
+    fn value(&self) -> u32 {
+        self.value
+    }
+}
+
+impl<const START_YEAR: usize> Display for ReleaseYearVvo<START_YEAR> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.value)
     }
 }
